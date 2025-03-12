748ba8129072f2176488b90ae8986f4ea5c5ae67
Eric B <eric.berthiaume@gmail.com>
2025-03-12 16:39:37 +0100

    typos

--- apis.tf
+++ apis.tf
@@ -22,7 +22,7 @@
 #    disable_on_destroy = false
 #}
 
-resource "google_project_service" "apis" {
+resource "google_project_service" "api" {
     for_each = toset(local.apis)
     service = each.key
 
