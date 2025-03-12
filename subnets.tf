471d74a6c4f33999ab17d09dfae6410f22495550
Eric B <eric.berthiaume@gmail.com>
2025-03-12 15:47:05 +0100

    changes

--- subnets.tf
+++ subnets.tf
@@ -10,4 +10,18 @@ resource "google_compute_subnetwork" "public" {
 resource "google_compute_subnetwork" "private" {
     name                        = "private"
     ip_cidr_range               = "10.0.32.0/19"
+    region                      = local.region 
+    network                     = google_compute_network.vpc.id
+    private_ip_google_access    = true
+    stack_type                  = "IPV4_ONLY"
+
+    secondary_ip_range {
+        range_name      = "k8s-pods"
+        ip_cidr_range   = "172.16.0.0/14"
+    }
+
+    secondary_ip_range {
+        range_name      = "k8s-services"
+        ip_cidr_range   = "172.20.0.0/18"
+    }
 }
